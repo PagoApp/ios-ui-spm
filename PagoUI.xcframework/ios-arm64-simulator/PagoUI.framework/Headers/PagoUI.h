f9cb114fc671ab7e86efcd9d08fe0c268c8002cf
Gabi Chiosa <gabi@bittreatsoftware.com>
2022-06-14 19:12:09 +0300

    added sdk

---
+++ PagoUI.xcframework/ios-arm64-simulator/PagoUI.framework/Headers/PagoUI.h
@@ -0,0 +1,18 @@
+//
+//  PagoUI.h
+//  PagoUI
+//
+//  Created by Gabi Chiosa on 24.02.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PagoUI.
+FOUNDATION_EXPORT double PagoUIVersionNumber;
+
+//! Project version string for PagoUI.
+FOUNDATION_EXPORT const unsigned char PagoUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagoUI/PublicHeader.h>
+
+
